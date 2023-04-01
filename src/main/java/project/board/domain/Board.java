0732fea7145913bfec14fb505474efc5b8280f3c
Jihun-Kim <anam4561@naver.com>
2023-04-01 19:53:25 +0900

    fix : timestamp class 수정

--- src/main/java/project/board/domain/Board.java
+++ src/main/java/project/board/domain/Board.java
@@ -19,20 +19,18 @@ public class Board {
     private String pwd;
     private String email;
     private String title;
-    private Timestamp writedate;
     private Long readed;
     private String content;
 
     public Board() {
     }
 
-    public Board(String bId, String writer, String pwd, String email, String title, Timestamp writedate, Long readed, String content) {
+    public Board(String bId, String writer, String pwd, String email, String title, Long readed, String content) {
         this.bId = bId;
         this.writer = writer;
         this.pwd = pwd;
         this.email = email;
         this.title = title;
-        this.writedate = writedate;
         this.readed = readed;
         this.content = content;
     }
